824c95e869fc87d85bf964bedc4d89a46859a649
goyuyoung <yooou5369@naver.com>
2025-03-17 23:47:43 +0900

    회원가입 페이지 작업중

---
+++ src/main/resources/static/js/join.js
@@ -0,0 +1,16 @@
+/*
+$(function() {
+    $("#domain_list").change(function () {
+        var slected = $("#domain_list option:selected").val();
+        var selectedDomain = $('#selectDomain');
+        if(slected !== "type") {
+            selectedDomain.val(slected);
+            selectedDomain.attr("disabled","true")
+        } else {
+            selectedDomain.val("");
+            selectedDomain.removeAttr("disabled");
+        }
+    })
+});
+*/
+
