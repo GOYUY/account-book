a5f12e165d3917788061e45920ffc781be000bac
goyuyoung <yooou5369@naver.com>
2025-03-20 18:32:23 +0900

    Front: fullcalendar.js 추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,31 +1,7 @@
-/*
-window.addEventListener("DOMContentLoaded",() => {
-    const nav = new Nav("nav");
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
 });
-
-class Nav {
-    constructor(qs) {
-        this.el = document.querySelector(qs);
-        this.expanded = true;
-        this.expandBtn = null;
-        this.timeout = null;
-        this.init();
-    }
-    init() {
-        this.expandBtn = this.el?.querySelector("[data-expand]");
-        this.expandBtn?.addEventListener("click",this.toggleSize.bind(this));
-    }
-    toggleSize() {
-        this.expanded = !this.expanded;
-        this.el.setAttribute("data-expanded", this.expanded);
-        this.expandBtn.setAttribute("aria-expanded", this.expanded);
-
-        const label = this.expanded ? "Collapse" : "Expand";
-        const timeoutValue = this.expanded ? 0 : 300;
-
-        clearTimeout(this.timeout);
-        this.timeout = setTimeout(() => {
-            this.el.querySelector("[data-expand-label]").innerText = label;
-        },timeoutValue);
-    }
-}*/
